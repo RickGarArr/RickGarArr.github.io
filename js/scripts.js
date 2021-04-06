d4ef66741a9310ca963227ac1e29e34de331f6ae
Ricardo <ricardogarciarreola@gmail.com>
2021-04-05 23:22:02 -0500

    mejoras al diseño

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,70 @@
-$(document).ready(()=>{
-    myOverflow();
-    $(window).resize(()=>{
-        myOverflow();
+$(document).ready(function() {
+    
+    "use strict";
+
+    // functions
+    anchor_links();
+    arrow_click();
+    animate_text();
+    navbar_scroll();
+
+    $(window).load('body', function(){
+        setTimeout(()=>{
+            $('.preloader').addClass('loaded');
+        }, 1500);
     });
 
-    $(window).on("orientationchange",()=>{
-        myOverflow();
-    });
-
-    $('.nav-header a').each((i, a) => {
-        if (i != 4) {   
-            a.addEventListener('click', (e)=>{
-                e.preventDefault();
-                $('html, body').animate({
-                    scrollTop: $(`${e.target.getAttribute('href')}`).offset().top - 24
-                }, 200);
-            });
-        }
-    });
 });
 
-function myOverflow() {
-    let scrollHeight = $(document).height() - $(window).height(); // 1328 - 100 ; x - 1
-    let value = parseFloat(101 / scrollHeight);
-    $(window).scroll(()=>{
-        let scroll = parseInt($(window).scrollTop());
-        if (scroll == scrollHeight) {
-            $('#barra').css({
-                'width': '100%'
-            }); 
+function anchor_links() {
+    "use strict";
+    $('.nav-header a').off().on('click', function(event) {
+        const element = $(this);
+        const url = element.attr('href');
+        if( url.charAt(0) === '#' ) {
+            $('html, body').animate({
+                scrollTop: $(url).offset().top - 75
+            }, 500);
         }
-        $('#barra').css({
-            'width': `${parseInt(scroll * value)}% `
-        });
+    });
+}
+
+
+function animate_text() {
+    "use strict";
+    const animatedText = $('.animate_text span');
+    animatedText.typed({
+        strings: ["Desarrollador <strong>Web</strong>", "<strong>Front</strong>End", "Back<strong>End</strong>"],
+        loop: true,
+        startDelay: 500,
+        backDelay: 1000,
+        typeSpeed: 75
+    });
+}
+
+function arrow_click() {
+    "use strict";
+
+    $('.arrow-wrap a').on('click', function(event){
+        const element = $(this);
+        const url = element.attr('href');
+        if(url.charAt(0) === '#' ) {
+            $('html, body').animate({
+                scrollTop: $(url).offset().top - 75
+            }, 500);
+        }
+    });
+}
+
+function navbar_scroll() {
+    "use strict";
+    $(window).on('scroll', function() {
+        const navBar = $('.nav-header');
+        const windowScroll = $(window).scrollTop();
+        if (windowScroll > 16) {
+            navBar.addClass('animate');
+        } else {
+            navBar.removeClass('animate');
+        }
     });
 }
